bb9cc55a79193adb54b1be183d82e9485e48bb5e
Khalid Zoabi <kzoabi@outlook.com>
2022-04-23 19:32:26 +0000

    Adding routes to serve SVGs to the frontend

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import open from 'open'
 
+import * as svgs from './svgs'
+
 export async function startServer (resources: Array<{ id: number, path: string, content: string }>) {
   const app = express()
   app.get('/search', (req, res, _next) => {
@@ -11,6 +13,78 @@ export async function startServer (resources: Array<{ id: number, path: string, 
       return res.json(resources.filter(resource => resource.path.toLowerCase().includes(lowercase) || resource.content.toLowerCase().includes(lowercase)))
     }
     return res.status(400).send('400 - Bad Request')
+  })
+  app.get('/img/logo', (_req, res, _next) => {
+    res.contentType('image/svg+xml')
+    res.send(svgs.CloudfsLogoSvg())
+  })
+  app.get('/icons/:type', (req, res, _next) => {
+    const type = req.params.type
+    if (type === 'alarms') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAmazonCloudWatchAlarm48LightSvg())
+    }
+    if (type === 'apis') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonAPIGateway48Svg())
+    }
+    if (type === 'buckets') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAmazonSimpleStorageServiceS3Standard48LightSvg())
+    }
+    if (type === 'canaries') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAmazonCloudWatchSynthetics48LightSvg())
+    }
+    if (type === 'certificates') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAWSCertificateManager48Svg())
+    }
+    if (type === 'distributions') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonCloudFront48Svg())
+    }
+    if (type === 'domains') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAmazonRoute53HostedZone48LightSvg())
+    }
+    if (type === 'functions') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAWSLambdaLambdaFunction48LightSvg())
+    }
+    if (type === 'instances') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAmazonEC2Instance48LightSvg())
+    }
+    if (type === 'parameters') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAWSSystemsManagerParameterStore48LightSvg())
+    }
+    if (type === 'queues') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonSimpleQueueService48Svg())
+    }
+    if (type === 'regions') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResDisk48LightSvg())
+    }
+    if (type === 'stacks') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ResAWSCloudFormationStack48LightSvg())
+    }
+    if (type === 'tables') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonDynamoDB48Svg())
+    }
+    if (type === 'topics') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonSimpleNotificationService48Svg())
+    }
+    if (type === 'vpcs') {
+      res.contentType('image/svg+xml')
+      return res.send(svgs.ArchAmazonVirtualPrivateCloud48Svg())
+    }
+    return res.status(404).send('404 - Not Found')
   })
   const port = 3000
   const url = `http://localhost:${port}/`
